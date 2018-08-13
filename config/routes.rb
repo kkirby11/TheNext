ca3dd61a15eeaf5b872c0bd3c7d0e5a2679bcbc7
kristina kirby <kristinakirby@mac.com>
2018-08-13 11:07:09 -0600

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/index'
+  end
+
+  namespace :api do
+    get 'users/update'
+  end
+
+  namespace :api do
+    get 'users/show'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  namespace :api do
+    get 'users/destroy'
+  end
+
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
     #API ROUTES SHOULD GO HERE
