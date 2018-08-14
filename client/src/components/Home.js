4051fb568d1df360a281d08b288e8d34b282f48e
Alan Bischoff <alanlbischoff@gmail.com>
2018-08-14 15:41:29 -0600

    home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 class Home extends Component {
   render() {
     return (
+      <div>
       <Header as='h1' textAlign='center'>Home Component</Header>
+      <LinkBoxWrapper>
+      <LinkBox textalign='center'>
+        I Own A Business
+      </LinkBox>
+      <LinkBox>
+        I Lead A team
+      </LinkBox>
+      <LinkBox>
+        I Have An Idea
+      </LinkBox>
+      </LinkBoxWrapper>
+      </div>
     );
   }
 }
+
+const LinkBox = styled.div`
+height: 450px;
+width: 250px;
+border: 3px solid black;
+`
+const LinkBoxWrapper = styled.div`
+display: flex;
+justify-content: space-around;
+flex-content: wrap;
+`
 
 export default Home;
